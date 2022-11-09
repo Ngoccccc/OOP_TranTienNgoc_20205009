56258197f5294da6c9d55f03895bbb1fac1c1f58
Ngoccccc <trantienngoc2911@gmail.com>
2022-11-09 21:04:04 +0700

    done lab02

--- lab02/AimsProject/src/mypackage/Aims.java
+++ lab02/AimsProject/src/mypackage/Aims.java
@@ -2,6 +2,21 @@ package mypackage;
 
 public class Aims {
     public static void main(String[] args) {
+        Cart anOrder = new Cart();
 
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 82, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        System.out.println("Total Cost is:");
+        System.out.println(anOrder.totalCost());
+        anOrder.removeDigitalVideoDisc(dvd3);
+        System.out.println("Total Cost is:");
+        System.out.println(anOrder.totalCost());
     }
 }
--- lab02/AimsProject/src/mypackage/Cart.java
+++ lab02/AimsProject/src/mypackage/Cart.java
@@ -2,5 +2,36 @@ package mypackage;
 
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
-    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+            System.out.println("The cart is full");
+        } else {
+            itemsOrdered[qtyOrdered++] = disc;
+            System.out.println("Add completed");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                for (int j = i; j < qtyOrdered - 1; ++j) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                qtyOrdered--;
+                break;
+            }
+        }
+        System.out.println("Remove completed");
+    }
+
+    public float totalCost() {
+        float ans = 0.0f;
+        for (int i = 0; i < qtyOrdered; i++) {
+            ans += itemsOrdered[i].getCost();
+        }
+        return ans;
+    }
 }
