e669e4373b090e222d0d0544cdcf60e11785f4fb
Ngoccccc <trantienngoc2911@gmail.com>
2022-11-22 17:06:19 +0700

    class-members

--- lab02/AimsProject/src/mypackage/DigitalVideoDisc.java
+++ lab02/AimsProject/src/mypackage/DigitalVideoDisc.java
@@ -6,6 +6,12 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    private int id;
+    private static int nbDigitalVideoDiscs = 0;
+
+    public int getId() {
+        return id;
+    }
 
     public String getTitle() {
         return title;
@@ -33,12 +39,16 @@ public class DigitalVideoDisc {
 
     public DigitalVideoDisc(String title) {
         this.title = title;
+        id = nbDigitalVideoDiscs;
+        nbDigitalVideoDiscs++;
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        id = nbDigitalVideoDiscs;
+        nbDigitalVideoDiscs++;
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -47,6 +57,8 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        id = nbDigitalVideoDiscs;
+        nbDigitalVideoDiscs++;
     }
 
     public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -54,6 +66,8 @@ public class DigitalVideoDisc {
         this.category = category;
         this.director = director;
         this.cost = cost;
+        id = nbDigitalVideoDiscs;
+        nbDigitalVideoDiscs++;
     }
 
 }
